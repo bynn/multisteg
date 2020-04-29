570f81d34c8f13640b011e0dcd2ae356b80ea135
bynn <bynnbynn@gmail.com>
2020-04-29 00:20:57 -0400

    complete

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,13 @@ use std::collections::HashMap;
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() <= 2 { panic!("needs more arguments"); }
-    println!("{:?}", args);
+    // println!("{:?}", args);
     let numthread: u8 = args[1].parse::<u8>().unwrap();
-    println!("number of threads: {}", numthread);
+    // validate_ppm("aux/in/dahlia-red-blossom-bloom-60597.png");
+    validate_ppm("aux/in/dahlia-red-blossom-bloom-60597.ppm");
+    // validate_ppm("aux/in/garden-rose-red-pink-56866.png");
+    // validate_ppm("../data/bible.txt");
+    // println!("number of threads: {}", numthread);
     // let mut bytes = match read_byte_by_byte(&args[2]) {
     //     Ok(line) => line,
     //     Err(_) => panic!("Error reading the file {}", &args[2]),
@@ -28,7 +32,7 @@ fn main() -> io::Result<()> {
 
     match args.len() {
         3 => {
-            println!("** DECODE ***");
+            // println!("** DECODE ***");
             let dir = &args[2];
             let mut names: Vec<String> = read_directory(dir);
             names.sort();
@@ -49,18 +53,28 @@ fn main() -> io::Result<()> {
 
             let (tx, rx) = mpsc::channel();
 
-            for x in 1..=threadsize {
+            for _ in 1..=threadsize {
                 let (counter, tx) = (Arc::clone(&counter), tx.clone());
                 let pairs = pairs.clone();
 
                 let handle = thread::spawn(move || {
-                    println!("spawning thread #{}", x);
-                    let mut counter = counter.lock().unwrap();
-                    let pair = &pairs[*counter];
-                    println!("{:?}", pair);
-                    *counter += 1;
-                        // while pair.0 < pairs.len() {
-                        println!("decoding {:?} on thread #{}", pair, x);
+                    // println!("spawning thread #{}", x);
+                    loop {
+                        let mut pair: &(usize, String) = &(0, "".to_string());
+                        let mut counter = counter.lock().unwrap();
+                        let c: usize = *counter;
+                        if c < pairs.len() {
+                            pair = &pairs[*counter];
+                        }
+                        // println!("{:?}", pair);
+                        *counter += 1;
+                        drop(counter);
+
+                        if c >= pairs.len() {
+                            break;
+                        }
+
+                        // println!("decoding {:?} on thread #{}", pair, x);
                         let bytes = match read_byte_by_byte(&pair.1) {
                             Ok(line) => line,
                             Err(_) => panic!("Error reading the file {}", &pair.1),
@@ -68,7 +82,7 @@ fn main() -> io::Result<()> {
                         let head = heading(&bytes);
                         let s = decode(bytes[head..].to_vec());
                         tx.send((pair.0,s)).unwrap();
-                        // }
+                        }
 
                 });
                 handles.push(handle);
@@ -76,10 +90,10 @@ fn main() -> io::Result<()> {
 
             let mut got = 0;
             for received in &rx {
-                println!("got: {}", received.0);
+                // println!("got: {}", received.0);
                 ret.push(received);
                 got += 1;
-                if got == pairs.len() { break };
+                if got >= pairs.len() { break };
             }
 
             for handle in handles {
@@ -88,7 +102,7 @@ fn main() -> io::Result<()> {
 
             ret.sort();
             for retval in &ret {
-                println!("{}", retval.1);
+                print!("{}", retval.1[..retval.1.len()-1].to_string());
             }
         },
         5 => {
@@ -141,19 +155,35 @@ fn main() -> io::Result<()> {
 
                 let handle = thread::spawn(move || {
                     println!("spawning thread #{}", x);
-                    let mut counter = counter.lock().unwrap();
-                    let plan = &plans[*counter];
-                    *counter += 1;
-                    println!("encoding on thread #{}", x);
-                    let mut bytes = match read_byte_by_byte(&plan.1) {
-                        Ok(line) => line,
-                        Err(_) => panic!("Error reading the file {}", &plan.1),
-                    }; //bytes is Vec<u8>;
-                    let count = plan.0 as usize;
-                    let head = heading(&bytes);
-                    let msg = plan.2.to_string();
-                    let ppm = encode(&mut bytes, msg, head);
-                    tx.send((count, ppm)).unwrap();
+                    loop {
+                        let mut plan: &(usize, String, String) = &(0, "".to_string(), "".to_string());
+                        // {
+                            let mut counter = counter.lock().unwrap();
+                            let c: usize = *counter;
+                            if c < plans.len() {
+                                plan = &plans[*counter];
+                            }
+                            // println!("count: {} plans.len(): {}", count, plans.len());
+                            *counter += 1;
+                            drop(counter);
+                        // }
+                        // println!("counter: {}, plans.len(): {}", count, plans.len());
+
+                        if c >= plans.len() {
+                            break;
+                        }
+
+                        println!("encoding on thread #{} msg #{}", x, count);
+                        let mut bytes = match read_byte_by_byte(&plan.1) {
+                            Ok(line) => line,
+                            Err(_) => panic!("Error reading the file {}", &plan.1),
+                        }; //bytes is Vec<u8>;
+                        let count = plan.0 as usize;
+                        let head = heading(&bytes);
+                        let msg = plan.2.to_string();
+                        let ppm = encode(&mut bytes, msg, head);
+                        tx.send((count, ppm)).unwrap();
+                    }
                 });
                 handles.push(handle);
             }
@@ -166,7 +196,7 @@ fn main() -> io::Result<()> {
                 got += 1;
                 let mut file = fs::File::create(txt)?;
                 file.write_all(&received.1)?;
-                if got == 10 { break };
+                if got >= plans.len() { break };
             }
 
             for handle in handles {
@@ -197,8 +227,53 @@ fn read_directory(dir: &String) -> Vec<String> {
 
         format!("{}/{}", dir, file_name_as_string)
     })
-    .filter(|file_name_as_string| file_name_as_string.ends_with(".ppm"))
+    .filter(|file_name_as_string| validate_ppm(file_name_as_string))
     .collect::<Vec<String>>();
 
     return names;
+}
+
+fn validate_ppm(path_to_file: &str) -> bool {
+    let bytes = match read_byte_by_byte(path_to_file) {
+        Ok(line) => line,
+        Err(_) => panic!("Error reading the file {}", path_to_file),
+    };
+
+    // for byte in &bytes[10..15] {
+    //     print!("{}", *byte);
+    // }
+
+    let mut spaces: Vec<usize> = vec![];
+    let mut temp = 0;
+    for (i, pixel) in bytes.iter().enumerate() {
+        if *pixel == 10u8 || *pixel == 32u8 {
+            temp += 1;
+            spaces.push(i);
+            if temp == 4 {
+                break
+            }
+        }
+    }
+
+    if temp != 4 {
+        return false;
+    }
+
+    if bytes[0..3] == [80, 54, 10]  || bytes[0..3] == [80, 51, 10] {
+        if bytes[spaces[2]..spaces[3]+1] == [10, 50, 53, 53, 10] {
+            // println!("match");
+            return true;
+        }
+    }
+
+
+    // println!("{:?}", spaces);
+    // for space in spaces {
+    //     let p = bytes[..space].to_vec();
+    //     for x in p {
+    //         print!("{}", x as char);
+    //     }
+    //     bytes = bytes[space..].to_vec();
+    // }
+    return false;
 }
